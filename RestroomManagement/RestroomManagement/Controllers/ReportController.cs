e4b6ecb7456d8eb47cc66f42dc42971ac5bc1b2a
Mriganga Kakati <mriganga.kakati@hotmail.com>
2019-11-04 20:35:08 +0700

    changes comment lines code.

--- RestroomManagement/RestroomManagement/Controllers/ReportController.cs
+++ RestroomManagement/RestroomManagement/Controllers/ReportController.cs
@@ -35,58 +35,7 @@ namespace RestroomManagement.Controllers
                 return listx;
             }
         }
-        //public IList<Utility_Report_Result> GetSampleData()
-        //{
-        //    List<Utility_Report_Result> listx = new List<Utility_Report_Result>();
-        //    using (sdirecttestdbEntities1 test = new sdirecttestdbEntities1())
-        //    {
-        //        var result = test.Utility_Report().ToList();
-        //        foreach (var item in result.ToList())
-        //        {
-        //            listx.Add(new Utility_Report_Result
-        //            {
-        //                U_ID = item.U_ID,
-        //                Name = item.Name,
-        //                //CreatedDate = item.CreatedDate,
-        //                ReadingDate = item.ReadingDate,
-        //                IsDone = item.IsDone,
-        //                ShiftId = item.ShiftId,
-        //                ShiftName = item.ShiftName,
-        //                Feedback = item.Feedback,
-
-        //            });
-
-
-        //        }
-        //        return listx;
-        //    }
-        //}
-        //public IHttpActionResult GetAllStudents()
-        //{
-        //    IList<Utility_Report_Result> students = null;
-
-        //    using (var ctx = new sdirecttestdbEntities())
-        //    {
-        //        students = ctx.Utilities.Include("StudentAddress")
-        //                    .Select(item => new Utility_Report_Result()
-        //                    {
-        //                        //U_ID = item.U_ID,
-        //                        Name = item.Name,
-        //                        CreatedDate = item.CreatedDate,
-        //                        //IsDone = item.IsDone,
-        //                        //ShiftId = item.ShiftId,
-        //                        //ShiftName = item.ShiftName,
-        //                        //Feedback = item.Feedback
-        //                    }).ToList<Utility_Report_Result>();
-        //    }
-
-        //    if (students.Count == 0)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return Ok(students);
-        //}
+        
 
     }
 }
